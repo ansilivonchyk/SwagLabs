274b843d4655f9eb11cbd9fb56c6964db41b12c7
aklueva <aklueva@xbsoftware.com>
2025-04-12 17:07:28 +0200

    Initial commit

---
+++ cypress/e2e/login_page.js
@@ -0,0 +1,52 @@
+///<reference types="cypress"/>
+
+
+describe ('Login page', () => {
+
+    beforeEach(() => {
+        cy.visit('https://www.saucedemo.com/')
+      })
+
+
+    it('[Login_Page_01] Login form is displayed', () => {
+        cy.get('#user-name').should('be.visible')
+        cy.get('#password').should('be.visible')
+        cy.get('#login-button').should('be.visible')
+
+        cy.get('.login_credentials_wrap-inner div')
+        .should('have.length', 2)
+        .first().should('contain', 'Accepted usernames are:') //part of the text
+        .last().should('have.text', 'Password for all users:secret_sauce') //full text
+       
+        // cy.screenshot('[Login_Page_01]')
+    })
+
+    it('[Login_Page_02] validate empty data', () => {
+        
+        const submitButton = cy.get('input[type="submit"]')
+        submitButton.click()
+        
+        cy.get('.error-message-container.error')
+        .should('be.visible') 
+        .should('contain', 'Epic sadface: Username is required')
+    })
+
+    it.only('[Login_Page_03] should allow user to log in', () => {
+        const userName = cy.get('input[name="user-name"]')
+        userName.type('standard_user{enter}') 
+
+        const pass = 'secret_sauce'
+        cy.get('input[name="password"]').type(pass) 
+
+        cy.contains('Login').click()
+        
+        cy.url().should('include', '/inventory') 
+        cy.contains('Login').should('not.exist')
+    })
+
+
+
+    afterEach(() => {
+        cy.clearCookies()
+      })
+})
